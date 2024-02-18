48fe8c509d800064c564f0d19bfc26579472de89
Nurbolat <nurbolat@Nurbolats-MacBook-Air.local>
2024-02-19 02:04:10 +0600

    added logger

--- app/v1/core/logger.py
+++ app/v1/core/logger.py
@@ -1,4 +1,23 @@
-"""
-Didn't have much time to implement the logging
-We could have used Graylog or throw to stdout in case of Kubernetes
-"""
+import logging
+import os
+from datetime import datetime
+
+class DateFileHandler(logging.FileHandler):
+    def __init__(self, filename, mode='a', encoding=None, delay=False):
+        today = datetime.now().strftime("%Y-%m-%d")
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        self.filename = os.path.join(os.path.dirname(filename), f"{today}.log")
+        logging.FileHandler.__init__(self, self.filename, mode, encoding, delay)
+
+def setup_logger():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    file_handler = DateFileHandler('logs/')
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger = logging.getLogger('')
+    logger.addHandler(file_handler)
+
+setup_logger()
